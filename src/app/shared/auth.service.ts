23aba78fffe7a841e0dfcbec4c52ad8cb6807744
vinod prajapati <vinodprajapatimj@gmail.com>
2023-05-06 13:15:26 +0530

    done

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {  BehaviorSubject, Observable } from 'rxjs';
+import {  BehaviorSubject, Observable, catchError, throwError } from 'rxjs';
 import{Item} from './item';
 @Injectable({
   providedIn: 'root'
@@ -20,26 +20,48 @@ export class AuthService {
   constructor(private httpClient: HttpClient) { }
 
   getAll() {
-    return this.httpClient.get(this.apiURL + '/Users')
+    return this.httpClient.get(this.apiURL + '/Users').pipe(
+      catchError(this.errorHandler)
+    )
   }
 
   post(post:Item){
-    return this.httpClient.post<Item>(this.apiURL + '/Register', JSON.stringify(post), this.httpOptions)
+    return this.httpClient.post<Item>(this.apiURL + '/Register', JSON.stringify(post), this.httpOptions).pipe(
+      catchError(this.errorHandler)
+    )
   }
-  login(post:any){
-    return this.httpClient.get<any>(this.apiURL + `/login?user_email=` + post.user_email + `&user_pwd=` + post.user_pwd )
+  login(Item: { user_email: string; user_pwd: string; }){
+    return this.httpClient.get<any>(this.apiURL + `/login?user_email=` + Item.user_email + `&user_pwd=` + Item.user_pwd ).pipe(
+      catchError(this.errorHandler)
+    )
   }
 
   find(user_id:number)  {
-    return this.httpClient.get<any>(this.apiURL + '/user_detail?' + 'user_id=' + user_id)
+    return this.httpClient.get<any>(this.apiURL + '/user_detail?' + 'user_id=' + user_id).pipe(
+      catchError(this.errorHandler)
+    )
   }
 
-  update(post: any) {
-    return this.httpClient.get<any>(this.apiURL + '/update_user')
+  update(Item: any) {
+    return this.httpClient.get<Item>(this.apiURL + '/update_user').pipe(
+      catchError(this.errorHandler)
+    )
   }
 
   delete(user_id: number){
-    return this.httpClient.get(this.apiURL + '/remove_user' + user_id)
+    return this.httpClient.get(this.apiURL + '/remove_user' + user_id).pipe(
+      catchError(this.errorHandler)
+    )
   }
 
+  errorHandler(error: { error: { message: string; }; status: string; message: string; }) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+ }
 }
--- src/app/shared/view/view.component.ts
+++ src/app/shared/view/view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-view',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view.component.css']
 })
 export class ViewComponent implements OnInit {
-
-  constructor() { }
+public user : any
+  constructor(
+    public router: ActivatedRoute,
+    public auth : AuthService,
+    public routers :Router
+  ) {   }
 
   ngOnInit(): void {
+    let user_id = this.router.snapshot.params['itemId'];
+    this.auth.find(user_id).subscribe((data: any) => {
+      this.user= data.data;
+      console.log(data)
+    })
+  }
+
+  onBack() {
+    this.routers.navigate(['/dashboard']);
   }
 
 }
